1fec5e6e38a2f742bd44f68cfdfdbe1538d90dcf
Yandiev <azizdavronov2005@gmail.com>
2025-07-25 18:16:55 +0500

    then

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,6 @@ import { PuchasedCourseModule } from './entities/puchased-course/purchased-cours
     HomeworkModule,
     ExamModule,
     VerificationModule,
-    
     AuthModule,
     PuchasedCourseModule,
   ],
