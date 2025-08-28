5e3fdb45c966e950079b5b1ef0ca0f1cc12e711f
Yandiev <azizdavronov2005@gmail.com>
2025-08-28 14:37:40 +0500

    els

--- src/common/seeders/seeder.service.ts
+++ src/common/seeders/seeder.service.ts
@@ -19,14 +19,15 @@ export class SeederService implements OnModuleInit {
     const role = UserRole.ADMIN; // UserRole enumdagi qiymat
     const image = null; // yoki biror URL string
 
-    const existingUser = await this.prisma.user.findUnique({
+    const user = await this.prisma.user.findUnique({
       where: { phone },
     });
+    let mentorId = 0
 
-    if (!existingUser) {
+    if (!user) {
       const hashedPassword = await bcrypt.hash(password, 10);
-
-      let {id:mentorId} = await this.prisma.user.create({
+      
+      let user = await this.prisma.user.create({
         data: {
           phone,
           password: hashedPassword,
@@ -35,16 +36,20 @@ export class SeederService implements OnModuleInit {
           image,
         },
       });
-      let courseCategory = [
-        { "name": "Frontend" },
-        { "name": "Backend" },
-        { "name": "DevOps" },
-        { "name": "UI/UX Design" },
-        { "name": "Data Science" },
-        { "name": "Mobile Development" }
-      ]
-      
+      mentorId = user.id
 
+      console.log('✅ Superadmin user created!');
+    } else {
+      console.log('ℹ️ Superadmin user already exists.');
+    }
+    let courseCategory = [
+        { name: 'Frontend' },
+        { name: 'Backend' },
+        { name: 'DevOps' },
+        { name: 'UI/UX Design' },
+        { name: 'Data Science' },
+        { name: 'Mobile Development' },
+      ];
 
       const categories = await Promise.all([
         this.prisma.courseCategory.create({ data: { name: 'Frontend' } }),
@@ -52,62 +57,67 @@ export class SeederService implements OnModuleInit {
         this.prisma.courseCategory.create({ data: { name: 'DevOps' } }),
         this.prisma.courseCategory.create({ data: { name: 'UI/UX Design' } }),
         this.prisma.courseCategory.create({ data: { name: 'Data Science' } }),
-        this.prisma.courseCategory.create({ data: { name: 'Mobile Development' } }),
+        this.prisma.courseCategory.create({
+          data: { name: 'Mobile Development' },
+        }),
       ]);
 
       const courses = [
-  {
-    name: "Frontend Dasturlash",
-    about: "HTML, CSS, JavaScript va React bo'yicha to'liq kurs.",
-    price: 1500000,
-    level: CourseLevel.BEGINNER,
-    categoryId: categories[0].id,
-    mentorId: mentorId,
-    banner: "https://images.unsplash.com/photo-1506744038136-46273834b3fb", // rasm URL
-    introVideo: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4" // video URL
-  },
-  {
-    name: "Backend Dasturlash",
-    about: "Node.js, NestJS va PostgreSQL orqali backend ishlab chiqish.",
-    price: 1800000,
-    level: CourseLevel.INTERMEDIATE,
-    categoryId: categories[1].id,
-    mentorId: mentorId,
-    banner: "https://images.unsplash.com/photo-1498050108023-c5249f4df085",
-    introVideo: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4"
-  },
-  {
-    name: "Mobil Ilovalar",
-    about: "Flutter yordamida Android va iOS ilovalar yaratish.",
-    price: 2000000,
-    level: CourseLevel.BEGINNER,
-    categoryId: categories[2].id,
-    mentorId: mentorId,
-    banner: "https://images.unsplash.com/photo-1517433456452-f9633a875f6f",
-    introVideo: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4"
-  },
-  {
-    name: "Data Science",
-    about: "Python, Pandas, NumPy, Machine Learning va Data Visualization.",
-    price: 2500000,
-    level: CourseLevel.ADVANCED,
-    categoryId: categories[3].id,
-    mentorId: mentorId,
-    banner: "https://images.unsplash.com/photo-1522071820081-009f0129c71c",
-    introVideo: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4"
-  }
-];
+        {
+          name: 'Frontend Dasturlash',
+          about: "HTML, CSS, JavaScript va React bo'yicha to'liq kurs.",
+          price: 1500000,
+          level: CourseLevel.BEGINNER,
+          categoryId: categories[0].id,
+          mentorId: user?.id || mentorId,
+          banner:
+            'https://images.unsplash.com/photo-1506744038136-46273834b3fb', // rasm URL
+          introVideo:
+            'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4', // video URL
+        },
+        {
+          name: 'Backend Dasturlash',
+          about: 'Node.js, NestJS va PostgreSQL orqali backend ishlab chiqish.',
+          price: 1800000,
+          level: CourseLevel.INTERMEDIATE,
+          categoryId: categories[1].id,
+          mentorId: user?.id || mentorId,
+          banner:
+            'https://images.unsplash.com/photo-1498050108023-c5249f4df085',
+          introVideo:
+            'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4',
+        },
+        {
+          name: 'Mobil Ilovalar',
+          about: 'Flutter yordamida Android va iOS ilovalar yaratish.',
+          price: 2000000,
+          level: CourseLevel.BEGINNER,
+          categoryId: categories[2].id,
+          mentorId: user?.id || mentorId,
+          banner:
+            'https://images.unsplash.com/photo-1517433456452-f9633a875f6f',
+          introVideo:
+            'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4',
+        },
+        {
+          name: 'Data Science',
+          about:
+            'Python, Pandas, NumPy, Machine Learning va Data Visualization.',
+          price: 2500000,
+          level: CourseLevel.ADVANCED,
+          categoryId: categories[3].id,
+          mentorId:  user?.id || mentorId,
+          banner:
+            'https://images.unsplash.com/photo-1522071820081-009f0129c71c',
+          introVideo:
+            'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4',
+        },
+      ];
 
-      await this.prisma.course.createMany({
-        data:courses
-      })
-      console.log(categories,"qoshildi");
-      
-
-      console.log('✅ Superadmin user created!');
-    } else {
-      console.log('ℹ️ Superadmin user already exists.');
-    }
+      for (const course of courses) {
+        await this.prisma.course.create({ data: course });
+      }
+      console.log(categories, 'qoshildi');
   }
 
   async onModuleInit() {
